eae16aa109c3d5c77c8afedd7b20142e84f23615
ARIA <gui.aria1@gmail.com>
2016-03-12 17:00:02 +0700

    // test test

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ void loadProblem(char load_input[300], int n)
     if(!strcmp(load_input, "PRIME")) {
         primeProblem(n);
     }
-    //bla bla bla
+    //bla bla bla asdasdasdasd
 }
 
 int main(int argc, const char * argv[]) {
