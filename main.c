1608d594e6bfd379b6567a09196b77b9df827e32
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2016-03-12 16:01:33 +0700

    Add print name

--- main.c
+++ main.c
@@ -37,6 +37,7 @@ int main(int argc, const char * argv[]) {
     printf("n:");
     scanf("%d", &n_input);
     
+    printf("I'm Nuthyll");
     loadProblem(load_input, n_input);
     
     return 0;
